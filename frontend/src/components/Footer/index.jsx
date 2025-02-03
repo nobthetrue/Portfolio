b41307db46758975400fbf8e7c9005c579468fcf
Gianni Cavanna <giannicavanna74@gmail.com>
2025-02-03 21:43:30 +0100

    Suppression fichier

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -3,7 +3,7 @@ import './index.scss'
 function Footer () {
     return <footer className='footer'>
         <nav className='nav-footer'>
-            <a href="/DetailMessage.pdf" download="DetailMessage.pdf" className='cat-footer'>.télécharger mon CV</a>
+            <a href="/DetailMessage.pdf" download="" className='cat-footer'>.télécharger mon CV</a>
             <a href="https://www.instagram.com/giannihc/?hl=fr" className='cat-footer'>.instagram</a>
             <a href="https://www.linkedin.com/in/gianni-cavanna" className='cat-footer'>.linkedIn</a>
         </nav>
