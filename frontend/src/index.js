4cda5f3a6a58890b23237a4ab7480897ed38e53c
Gianni Cavanna <giannicavanna74@gmail.com>
2025-02-04 09:37:58 +0100

    Second deplaoy

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Accueil from './pages/Accueil';
 import ProjectPage from './pages/Projet';
 import Contact from './pages/Contact';
