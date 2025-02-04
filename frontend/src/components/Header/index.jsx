232119962e025a44eed788a6f18512f7c566986d
Gianni Cavanna <giannicavanna74@gmail.com>
2025-02-04 09:51:28 +0100

    Third deploy

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,16 +1,18 @@
-import './header.scss'
+import { Link } from 'react-router-dom';
+import './header.scss';
 
-function Header () {
-    return <header> 
-                <h1 className='header-title'>.WELCOME</h1>
-                <nav className='nav-header'>
-                    <a href="/" className='nav-cat'>.Accueil</a>
-                    <a href='/Projet' className='nav-cat'>.Projets</a>
-                    {/* <a href="/Formation" className='nav-cat'>.Formation</a> */}
-                    <a href="/Contact" className='nav-cat'>.Contact</a>
-                </nav>
+function Header() {
+    return (
+        <header> 
+            <h1 className='header-title'>.WELCOME</h1>
+            <nav className='nav-header'>
+                <Link to="/" className='nav-cat'>.Accueil</Link>
+                <Link to="/projet" className='nav-cat'>.Projets</Link>
+                {/* <Link to="/formation" className='nav-cat'>.Formation</Link> */}
+                <Link to="/contact" className='nav-cat'>.Contact</Link>
+            </nav>
         </header>
-    
+    );
 }
 
-export default Header
+export default Header;
